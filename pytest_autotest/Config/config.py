154a1828ae7f0f00312a91f4d17584a89579d87a
闫子强 <yanziqiang@yanziqiangdeMacBook-Pro.local>
2021-03-05 18:39:07 +0800

    something

--- pytest_autotest/Config/config.py
+++ pytest_autotest/Config/config.py
@@ -1,3 +1,6 @@
+"""
+python ConfigParser()模块简介：ConfigParser模块用于生成和修改常见配置文档，文件格式见config.ini
+"""
 from configparser import ConfigParser
 import os
 
@@ -11,12 +14,12 @@ class ReadConfig():
     host = 'host'
 
     def __init__(self):
-        self.config_file = os.path.dirname(__file__) + '/config.ini'
+        self.config_file = os.path.dirname(__file__) + '/config.ini' # 配置文件地址
         self.config = ConfigParser()
-        self.config.read(self.config_file)
+        self.config.read(self.config_file) # 读取文件配置
 
-        self.tester = self.get_config(self.title, self.tester)
-        self.host = self.get_config(self.title, self.host)
+        self.tester = self.get_config(self.title, self.tester) # 测试人
+        self.host = self.get_config(self.title, self.host) # 测试地址
 
     def get_config(self, title, value):
         value = self.config.get(title, value)
--- pytest_autotest/Params/tools.py
+++ pytest_autotest/Params/tools.py
@@ -6,28 +6,35 @@ import os.path
 # 获取yaml中的数据转化为python dic
 def parse():
     param_paths = ['/User']
-    pages = {}
+    pages = {} # 定义空字典后续存放解析后的yaml数据
 
     for param_path in param_paths:
-        file_path = os.path.dirname(__file__) + param_path
+        file_path = os.path.dirname(__file__) + param_path # 拼接路由
+
+        # 文件、目录遍历器，遍历目标目录，返回三元组(root,dirs,files)，
+        # root是当前正在遍历的这个文件夹本身的地址
+        # dirs是一个list，内容是该文件目录下的所有一级目录名字
+        # files是一个list，内容是该目录下的所有一级文件
         for root, dirs, files in os.walk(file_path):
             for name in files:
                 watch_file_path = os.path.join(root, name)
+                # 读取yaml文件
                 with open(watch_file_path, encoding='utf-8') as f:
-                    page = yaml.safe_load(f)
+                    page = yaml.safe_load(f) # 只解析基本的yaml标记，用来保证代码的安全性
                     # print(page)
                     # print(type(page))
-                    pages.update(page)
+                    pages.update(page) # 修改当前字典
     return pages
 
 def get_page_list():
-    page_list = {}
-    pages = parse()
+    page_list = {} # 定义空字典
+    pages = parse() # 调用函数，返回字典
     for page, value in pages.items():
-        parameters = value['parameters']
+        parameters = value['parameters'] # 取字典中存放的parameters的数据
         data_list = []
 
         for parameter in parameters:
             data_list.append(parameter)
-        page_list[page] = data_list
+        page_list[page] = data_list # params以列表方式存放
+
     return page_list
